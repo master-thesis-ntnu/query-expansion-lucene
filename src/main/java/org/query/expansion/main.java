1a99a4eceb406d8a40fca4746a221c00fba42481
Jonatan Lund <jonatan.lund.92@gmail.com>
2017-01-25 15:35:35 +0100

    Initial commit

---
+++ src/main/java/org/query/expansion/main.java
@@ -0,0 +1,10 @@
+package org.query.expansion;
+
+import org.apache.lucene.index.IndexReader;
+
+public class main {
+    public static void main(String[] args) {
+        IndexReader indexReader;
+        System.out.println("Test");
+    }
+}
